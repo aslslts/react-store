ba0560e8f46f8bfe4dbb46cc2de8ae920aadb681
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-25 20:49:17 +0300

    Reactstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
+import { Container } from "reactstrap";
+import { Col } from "reactstrap";
 
 function App() {
   return (
     <div>
-      <Navi />
-      <CategoryList />
-      <ProductList />
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
