957e68964ee524bb36b62c533ba6068ba415b56c
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-25 23:09:27 +0300

    Event added

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -8,19 +8,26 @@ export default class CategoryList extends Component {
       { categoryId: 1, categoryName: "Beverages" },
       { categoryId: 2, categoryName: "Condiments" },
     ],
+    currentCategory: "",
   };
-
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem key={category.categoryId}>
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.categoryId}
+            >
               {category.categoryName}
             </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
