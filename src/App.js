fa18b6bf8e5bb35b4e129bd08128aa6a9720685a
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-25 23:25:22 +0300

    React - Event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
@@ -6,26 +6,38 @@ import ProductList from "./ProductList";
 import { Container } from "reactstrap";
 import { Col } from "reactstrap";
 
-function App() {
-  let productInfo = { title: "Product List" };
-  let categoryInfo = { title: "Category List" };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = { currentCategory: "" };
 
-export default App;
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+  render() {
+    let productInfo = { title: "Product List" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
