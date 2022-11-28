1d8c0cb44c09a026eee0935b46292e0e22501c1c
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-28 16:26:01 +0300

    Listing of All Products

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import { Container } from "reactstrap";
 import { Col } from "reactstrap";
 
 export default class App extends Component {
-  state = { currentCategory: "" };
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
 
   changeCategory = (category) => {
     this.setState({ currentCategory: category.categoryName });
   };
+
+  getProducts = () => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
   render() {
     let productInfo = { title: "Product List" };
     let categoryInfo = { title: "Category List" };
@@ -31,6 +42,7 @@ export default class App extends Component {
             </Col>
             <Col xs="9">
               <ProductList
+                products={this.state.products}
                 currentCategory={this.state.currentCategory}
                 info={productInfo}
               />
