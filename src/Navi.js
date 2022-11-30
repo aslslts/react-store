ee2ffbdca218d75a809a0532cb952a5dfb84c76b
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-01 00:58:30 +0300

    fixed

--- src/Navi.js
+++ src/Navi.js
@@ -1,70 +1,3 @@
-// import React from "react";
-
-// import {
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   UncontrolledDropdown,
-//   DropdownToggle,
-//   DropdownMenu,
-//   DropdownItem,
-// } from "reactstrap";
-
-// export default class Navi extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.toggle = this.toggle.bind(this);
-//     this.state = {
-//       isOpen: false,
-//     };
-//   }
-//   toggle() {
-//     this.setState({
-//       isOpen: !this.state.isOpen,
-//     });
-//   }
-//   render() {
-//     return (
-//       <>
-//         <Navbar color="light" light expand="md">
-//           <NavbarBrand href="/">Northwind App</NavbarBrand>
-//           <NavbarToggler onClick={this.toggle} />
-
-//             <Collapse isOpen={this.state.isOpen} navbar style={{flexGrow: 0}}>
-//               <Nav className="ml-auto" navbar>
-//                 <NavItem>
-//                   <NavLink href="/components">Components</NavLink>
-//                 </NavItem>
-//                 <NavItem>
-//                   <NavLink href="https://github.com/reactstrap/reactstrap">
-//                     GitHub
-//                   </NavLink>
-//                 </NavItem>
-//                 <UncontrolledDropdown nav inNavbar>
-//                   <DropdownToggle nav caret>
-//                     Options - {this.props.cart.length}
-//                   </DropdownToggle>
-//                   <DropdownMenu right>
-//                     <DropdownItem>Option 1</DropdownItem>
-//                     <DropdownItem>Option 2</DropdownItem>
-//                     <DropdownItem divider />
-//                     <DropdownItem>Reset</DropdownItem>
-//                   </DropdownMenu>
-//                 </UncontrolledDropdown>
-//               </Nav>
-//             </Collapse>
-
-//         </Navbar>
-//       </>
-//     );
-//   }
-// }
-
 import React, { useState } from "react";
 import {
   Collapse,
@@ -95,9 +28,7 @@ function Navi(props) {
               <NavLink href="/components/">Components</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
+              <NavLink href="https://github.com/aslslts">GitHub</NavLink>
             </NavItem>
             <CartSummary
               removeFromCart={props.removeFromCart}
